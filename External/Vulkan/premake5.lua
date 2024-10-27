05f72abcc7bc37b76793d4249a712cf0a2f3cb35
Froncu <jakubfratczak204@gmail.com>
2024-10-27 17:30:09 +0100

    [FIX] link to the debug version of shaderc when in debug configuration

--- External/Vulkan/premake5.lua
+++ External/Vulkan/premake5.lua
@@ -1,3 +1,10 @@
 libdirs "$(VULKAN_SDK)/Lib"
-links { "vulkan-1.lib", "shaderc_combined.lib" }
+links "vulkan-1.lib"
+
+filter "configurations:debug"
+   links "shaderc_combinedd.lib"
+filter "configurations:distribute or configurations:release"
+   links "shaderc_combined.lib"
+filter {}
+
 includedirs "$(VULKAN_SDK)/Include"
