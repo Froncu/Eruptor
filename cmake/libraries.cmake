be543c53b45f0f76ee9a5e228208aa967e7d6ec1
Froncu <jakubfratczak204@gmail.com>
2025-02-18 22:35:07 +0100

    [FEATURE] fetch and link to imgui

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -6,7 +6,7 @@ set(FETCHCONTENT_QUIET FALSE)
 set(BUILD_SHARED_LIBS FALSE)
 set(SDL_TEST_LIBRARY FALSE)
 FetchContent_Declare(SDL
-   GIT_REPOSITORY https://github.com/libsdl-org/SDL
+   GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
    GIT_TAG release-3.2.4
    GIT_PROGRESS TRUE
    GIT_SHALLOW TRUE)
@@ -17,6 +17,27 @@ FetchContent_Declare(glm
    GIT_PROGRESS TRUE
    GIT_SHALLOW TRUE)
 
+FetchContent_Declare(imgui
+   GIT_REPOSITORY https://github.com/ocornut/imgui.git
+   GIT_TAG v1.91.8
+   GIT_PROGRESS TRUE
+   GIT_SHALLOW TRUE)
+
 FetchContent_Makeavailable(
    SDL
-   glm)
+   glm
+   imgui)
+
+add_library(imgui STATIC
+   ${imgui_SOURCE_DIR}/imgui.cpp
+   ${imgui_SOURCE_DIR}/imgui_demo.cpp
+   ${imgui_SOURCE_DIR}/imgui_draw.cpp
+   ${imgui_SOURCE_DIR}/imgui_tables.cpp
+   ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+   ${imgui_SOURCE_DIR}/backends/imgui_impl_vulkan.cpp
+   ${imgui_SOURCE_DIR}/backends/imgui_impl_sdl3.cpp)
+target_include_directories(imgui PUBLIC ${imgui_SOURCE_DIR})
+
+target_link_libraries(imgui
+   PUBLIC Vulkan::Vulkan
+   PUBLIC SDL3::SDL3)
