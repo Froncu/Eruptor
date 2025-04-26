3c90debe53d58c9dacd0c54e3a98a1b9003f4f8e
Jakub Fratczak <jakubfratczak204@gmail.com>
2025-04-26 23:11:39 +0200

    [BUILD] add `SDL3_image`

--- source/erupch/erupch.hpp
+++ source/erupch/erupch.hpp
@@ -19,6 +19,7 @@
 #include <glm/vec4.hpp>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
+#include <SDL3_image/SDL_image.h>
 #include <shaderc/shaderc.hpp>
 #include <vma/vk_mem_alloc.h>
 #include <vulkan/vulkan.hpp>
