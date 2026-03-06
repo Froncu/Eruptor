70ac6f4ddad8ae3db45cb8cd08d804b20a37ab87
Jakub Fratczak <jakubfratczak204@gmail.com>
2026-03-07 00:01:08 +0100

    [FEATURE] create an instance

--- source/core/application.cpp
+++ source/core/application.cpp
@@ -32,10 +32,45 @@ namespace eru
       glfwPollEvents();
    }
 
-   Application::Application()
+   Application::Application(std::string_view const name, std::uint32_t const version)
+      : instance_{ instance(name, version) }
    {
       Locator::provide<Application>(*this);
       Locator::provide<Logger>();
-      Locator::provide<Window>();
+      Locator::provide<Window>().change_title(name);
+   }
+
+   vk::raii::Instance Application::instance(std::string_view const name, std::uint32_t const version)
+   {
+      vk::ApplicationInfo const app_info{
+         .pApplicationName{ name.data() },
+         .applicationVersion{ version },
+         .pEngineName{ "eruptor" },
+         .engineVersion{ VK_MAKE_VERSION(0, 0, 0) },
+         .apiVersion{ vk::HeaderVersionComplete }
+      };
+
+      std::vector<char const*> extension_names;
+
+      {
+         std::uint32_t required_instance_extensions_count;
+         char const** const required_instance_extensions{
+            glfwGetRequiredInstanceExtensions(&required_instance_extensions_count)
+         };
+
+         extension_names.assign(required_instance_extensions,
+            required_instance_extensions + required_instance_extensions_count);
+      }
+
+      return {
+         vulkan_context_, {
+            .flags{},
+            .pApplicationInfo{ &app_info },
+            .enabledLayerCount{},
+            .ppEnabledLayerNames{},
+            .enabledExtensionCount{ static_cast<std::uint32_t>(extension_names.size()) },
+            .ppEnabledExtensionNames{ extension_names.data() }
+         }
+      };
    }
 }
