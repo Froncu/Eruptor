79ecb29ad1bb8c244f044f70700af243ccd9eb97
froncu <jakubfratczak204@gmail.com>
2026-04-25 14:42:57 +0200

    [CHORE] integrate clang tooling in the project

--- cmake/project_defaults.cmake
+++ cmake/project_defaults.cmake
@@ -1,9 +1,6 @@
 function(set_project_defaults TARGET_NAME)
    set_target_properties(${TARGET_NAME} PROPERTIES
       CXX_STANDARD 23
-      CXX_STANDARD_REQUIRED TRUE)
-
-   target_compile_options(${TARGET_NAME}
-      PRIVATE $<$<CXX_COMPILER_ID:GNU,Clang>:-Wall -Wextra -Wpedantic -Werror>
-      PRIVATE $<$<CXX_COMPILER_ID:MSVC>:/W4 /WX>)
+      CXX_STANDARD_REQUIRED TRUE
+      CXX_CLANG_TIDY clang-tidy --extra-arg=-fno-ms-extensions --extra-arg=-fno-ms-compatibility)
 endfunction()
